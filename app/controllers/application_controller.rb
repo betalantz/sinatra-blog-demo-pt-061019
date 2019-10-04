b6c1fe910639e8b6a51910fead748259f23b7a3c
betalantz <betalantz@gmail.com>
2019-10-04 13:40:33 -0700

    prevents unneccesary AR calls for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @user = User.find_by_id(session[:user_id])
+      @user ||= User.find_by_id(session[:user_id])
     end
   end
 
